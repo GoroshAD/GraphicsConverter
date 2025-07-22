49d3f8e9c9bc3ff0f213010bbc38632984c8a8ea
Artiomka Goroschko <nikto8748@gmail.com>
2025-07-22 19:30:38 +0300

    Fixed

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TextGraphicsConverter converter = new Converter(); // Создайте тут объект вашего класса конвертера
+        TextGraphicsConverter converter = new Converter();
 
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем
